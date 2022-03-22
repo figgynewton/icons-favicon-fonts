65d12bdea68738437374f729f73696587ec933bf
hannahnewton <hnewton17@yahoo.com>
2022-03-21 21:34:20 -0400

    update

--- app.js
+++ app.js
@@ -1,21 +1,8 @@
-let menu=document.getElementById("drop");
-let hb=document.getElementById("drop-content");
-
-hb.addEventListener("click",function (){
-    if(menu.style.display==="block") {
-        hb.innerHTML="<i class=\"fa-solid fa-bars\"></i>";
-        menu.style.display = "none";
+function myFunction() {
+    let x = document.getElementsByClassName("drop-content");
+    if (x.className === "drop") {
+        x.className += " responsive";
+    } else {
+        x.className = "drop";
     }
-    else {
-        menu.style.display = "block";
-        hb.innerHTML="<i class=\"fa-solid fa-bars\"></i>";
-    }
-});
-
-window.addEventListener("resize", function(){
-    if(window.matchMedia("(max-width: 600px)").matches)
-    {
-        menu.style.display="none";
-        hb.innerHTML="<i class=\"fa-solid fa-bars\"></i>";
-    }
-});
+}
