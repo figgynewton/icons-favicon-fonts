c3368740c023743023d9b38dfdf150c60b9610b9
hannahnewton <hnewton17@yahoo.com>
2022-03-21 22:38:00 -0400

    update

--- app.js
+++ app.js
@@ -1,8 +1,17 @@
-function myFunction() {
-    let x = document.getElementsByClassName("drop-content");
-    if (x.className === "drop") {
-        x.className += " responsive";
-    } else {
-        x.className = "drop";
+let menu=document.getElementById("menu");
+let hb=document.getElementById("menu-button");
+
+hb.addEventListener("click",function (){
+    if(menu.style.display==="none") {
+        menu.style.display = "block";
     }
-}
+    else {
+        menu.style.display = "none";
+    }
+});
+
+window.addEventListener("resize", function(){
+    if(window.matchMedia("(min-width: 600px)").matches){
+        menu.style.display="none";
+    }
+});
